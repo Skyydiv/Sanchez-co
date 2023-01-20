52aca559327f339de4d4e7258e7ca26bb029e816
Haya Mamlouk <hayamamlouk25@gmail.com>
2023-01-20 10:10:39 +0100

    modification constructeur Environnement et ajout de la methode afficheTab

--- classes/Environnement.py
+++ classes/Environnement.py
@@ -1,18 +1,32 @@
 import numpy
-from Robot import Robot
+from Objet import Robot
+
 
 class Environnement() :
     ''' L'environnement dans lequel se trouve le Robot'''
 
-    def __init__(self,n):
-        '''Constructeur de la classe Environnement, creer un tableau vide de robot de dimension nxn
+    def __init__(self,x,y):
+        '''Constructeur de la classe Environnement, creer un tableau vide de robot de x lignes et y colonnes
         
-        :param n: dimension du tableau
+        :param x: nombre de lignes
+        :param y: nombre de colonnes
         '''
-        self.tab=numpy.empty((n,n),Robot)
-        self.taille=n
+        self.tab=numpy.empty((int(x),int(y)),Robot)
+        self.nblignes=int(x)
+        self.nbcolonnes=int(y)
+
+    def afficheTab(self):
+        '''Affiche l'environnement dans la console'''
+        for ligne in self.tab:
+            for elt in ligne:
+                print(elt, end=", ")
+            print()
 
 
+
+#tests
+env=Environnement(5.3,4)
+env.afficheTab()
 
 
     
