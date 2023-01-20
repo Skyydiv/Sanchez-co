fe82b830eb59395a7bec88b868d4bb526c04c9a2
Elisa Li <elisali@MacBook-Air-de-Elisa.local>
2023-01-20 22:19:36 +0100

    ajout de la méthode avancer

--- classes/Objet.py
+++ classes/Objet.py
@@ -30,6 +30,52 @@ class Robot:
     '''
     if(self.orientation!=orientation):
           self.orientation=orientation
+  
+  def avancer(self, env, n):
+    '''Fais avancer le robot de n cases dans env si possible, renvoie un message sinon
+    :param n: le nombre de case à avancer
+    '''
+    if (self.orientation=="bas"):
+      newY=self.y-n
+      if (newY>=env.nblignes):
+        print("Le robot ne peut pas avancer")
+      if (estMur(x,newY)):
+        print("Le robot ne peut pas avancer")
+      else:
+        env.tab[self.x][self.y]=None
+        self.y=newY
+        deposer(env,x,newY)
+    if (self.orientation=="haut"):
+      newY=self.y+n
+      if (newY<0):
+        print("Le robot ne peut pas avancer")
+      if (estMur(x,newY)):
+        print("Le robot ne peut pas avancer")
+      else:
+        env.tab[self.x][self.y]=None
+        self.y=newY
+        deposer(env,x,newY)
+    if (self.orientation=="droite"):
+      newX=self.x+n
+      if (newX>=env.nbcolonnes):
+        print("Le robot ne peut pas avancer")
+      if (estMur(newX,y)):
+        print("Le robot ne peut pas avancer")
+      else:
+        env.tab[self.x][self.y]=None
+        self.x=newX
+        deposer(env,newX,y)
+    if (self.orientation=="gauche"):
+      newX=self.x-n
+      if (newX<0):
+        print("Le robot ne peut pas avancer")
+      if (estMur(newX,y)):
+        print("Le robot ne peut pas avancer")
+      else:
+        env.tab[self.x][self.y]=None
+        self.x=newX
+        deposer(env,newX,y)
+
 
   
 #tests
