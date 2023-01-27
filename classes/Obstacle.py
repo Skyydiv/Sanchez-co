92e392e7c97a6b9c293569eed6455d1536de84aa
Haya Mamlouk <hayamamlouk25@gmail.com>
2023-01-27 13:11:19 +0100

    creation de la class simulation

--- classes/Obstacle.py
+++ classes/Obstacle.py
@@ -1,4 +1,6 @@
 class Obstacle :
+    '''Obstacle qui peuvent être présent dans l'environnement'''
+    
     def __init__(self,x,y,h,d):
         '''Construteur de l'objet Obstacle qui initialise les coordonnées,l'hauteur, et la distance du sol
         :param x: coordonnée des abscisses
