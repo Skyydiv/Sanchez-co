96f22330a00193b37dc7b32a77c865230bf698ee
Haya Mamlouk <hayamamlouk25@gmail.com>
2023-01-27 12:33:26 +0100

    creation de class Obstacle

--- classes/Objet.py
+++ classes/Objet.py
@@ -4,8 +4,8 @@ class Robot:
     '''Constructeur de la classe Robot,avec des coordonnées par défaut (0,0) et une orientation=0 (par défaut) 
     (angle en radian) et une vitesse en forme d'un vecteur
     :param vitesse: liste de 2 élements [vx,vy] qui représente le vecteur de la vitesse du robot'''
-    self.x = 0
-    self.y = 0
+    self.x = 0.0
+    self.y = 0.0
     self.orientation=0
     self.vitesse=vitesse
   
