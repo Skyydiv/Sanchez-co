5f5eaff54e7d38312c5b89a33c74422b6f36b898
Haya Mamlouk <hayamamlouk25@gmail.com>
2023-01-27 12:21:26 +0100

    modifie construteur robot

--- classes/Objet.py
+++ classes/Objet.py
@@ -1,9 +1,11 @@
 class Robot:
 
-  def __init__(self, orientation):
-    '''Constructeur de la classe Robot,avec des coordonnées par défaut (0,0) et une orientation
-    :param orientation: Orientation du robot'''
+  def __init__(self,vitesse ):
+    '''Constructeur de la classe Robot,avec des coordonnées par défaut (0,0) et une orientation=0 (par défaut) 
+    (angle en radian) et une vitesse en forme d'un vecteur
+    :param vitesse: liste de 2 élements [vx,vy] qui représente le vecteur de la vitesse du robot'''
     self.x = 0
     self.y = 0
-    self.orientation=orientation
+    self.orientation=0
+    self.vitesse=vitesse
   
