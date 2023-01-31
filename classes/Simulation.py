cc77e9be638fd6d04af85df3ee7101e0e2270a03
Skyydiv <Corentin-fontaine@hotmail.com>
2023-01-31 17:19:31 +0100

    ajout de la méthode simu et update à simulation ainsi qu'import sleep

--- classes/Simulation.py
+++ classes/Simulation.py
@@ -1,6 +1,7 @@
 from Environnement import Environnement
 from Objet import Robot
 from Objet import Obstacle
+from time import sleep
 
 class Simulation :
     '''Simulation qui fait interagir le Robot avec son Environnement'''
@@ -13,3 +14,23 @@ class Simulation :
         self.robot=Robot()
         self.temps=temps
         self.delta=pas
+
+
+    def simu(self):
+        '''
+        Gère la simulation, c'est à dire le temps, et les appels aux fonctions de déplacement du robot et d'affichage de la simulation
+        '''
+        date=0
+        while date<self.temps:
+            self.update()
+            #appel d'affichage à changer si besoin et import view
+            #view.action(self) #je donne la simulation en paramètre pour permettre de récupérer les attributs de la simulation
+            date+=self.delta
+            
+            sleep(1) #attends 1 seconde
+
+    def update(self):
+        self.environnement.deplacer(self.robot)
+        
+
+
