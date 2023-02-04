1a2ec37d4fb934022e5479e3a98ed00e6feb5371
Skyydiv <Corentin-fontaine@hotmail.com>
2023-02-04 18:44:32 +0100

    Modification de Obstacle

--- classes/Objet.py
+++ classes/Objet.py
@@ -1,7 +1,7 @@
 class Robot:
 
   def __init__(self,vitesse, rayon, angle=0):
-    '''Constructeur de la classe Robot,représentation sous forme de cercle avec des coordonnées par défaut (0.1,0.1), une vitesse en forme d'un vecteur, un rayon une orientation (angle en radiant)
+    '''Constructeur de la classe Robot,représentation sous forme de cercle avec des coordonnées par défaut le coin haut gauche (rayon+0.1, rayon+0.1) , une vitesse en forme d'un vecteur, un rayon une orientation (angle en radiant)
     :param vitesse: liste de 2 élements [vx,vy] qui représente le vecteur de la vitesse du robot
     :param rayon: rayon de l'objet (en cm)
     :param orientation(optionel): angle (en radian) (par défaut, orientation=0) 
@@ -15,15 +15,17 @@ class Robot:
 class Obstacle :
   '''Obstacle qui peuvent être présent dans l'environnement'''
     
-  def __init__(self,x,y,h,d):
-      '''Construteur de l'objet Obstacle qui initialise les coordonnées,l'hauteur, et la distance du sol
-      :param x: coordonnée des abscisses
-      :param y: coordonnée des ordonnées
-      :param h: hauteur de l'obstacle
-      :paran d: distance du sol
+  def __init__(self,x,y,h,d,rayon):
+      '''Construteur de l'objet Obstacle, représenter par un cercle , qui initialise les coordonnées, la hauteur, et la distance du sol et le rayon.
+      :param x: coordonnée des abscisses (en cm)
+      :param y: coordonnée des ordonnées (en cm)
+      :param h: hauteur de l'obstacle (en cm)
+      :paran d: distance du sol (en cm)
+      :param rayon: rayon de l'obstacle
       '''
       self.x=x
       self.y=y
       self.hauteur=h
       self.distSol=d
+      self.rayon= rayon
   
