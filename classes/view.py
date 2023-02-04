8d7891d888479b16889a5dac9ba63422daad03f3
Haya Mamlouk <hayamamlouk25@gmail.com>
2023-02-04 18:21:05 +0100

    ajout modif view

--- classes/view.py
+++ classes/view.py
@@ -1,13 +1,24 @@
 from tkinter import *
+from tkinter import ttk
 
 class View :
     '''L'interface graphique de la simulation'''
+
     def __init__(self,simu):
         '''Constructeur de l'interface graphique de la simulation
         :param simu: simulation qu'on veut représenter graphiquement
         '''
+        self.root=Tk()
+        self.root.title("Simulation de Dexter")
+
         self.simu=simu
         self.robot=simu.environnement.robot
         self.obstacles=simu.environnement.ensemble_obstacles
         self.longueur=simu.environnement.coordsmax[0]
         self.largeur=simu.environnement.coordsmax[1]
+
+        #create canva
+        self.can=Canvas(self.root, bg="black",highlightbackground='white',highlightthickness=4, height=self.largeur, width=self.longueur)
+
+        #draw robot
+        can.create_arc(self.robot.x,self.robot.y,self.robot.rayon, self.robot.rayon, start=45, extent=270,fill="yellow")
