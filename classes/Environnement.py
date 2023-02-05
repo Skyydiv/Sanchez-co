662706e307422647a36ff820aaebadaddf500dbe
Skyydiv <Corentin-fontaine@hotmail.com>
2023-02-05 16:29:39 +0100

    ajout calculDistance à Environnement

--- classes/Environnement.py
+++ classes/Environnement.py
@@ -123,6 +123,16 @@ class Environnement() :
         print("-------------")
 
 
+    def calculDistance(objet1, objet2):
+        '''
+        Calcule la distance entre deux objets passer en paramètre, en prenant compte le rayon le rayon deces objets. Les objets peuvent être des robots ou des obstacles.
+        :param objet1 : robot/obstacle
+        :param objet2 : robot/obstacle
+        :return : valeur négative si les objects sont en collision (ne gère pas la hauteur)
+        :return : sinon valeur positive correspondant à la distance en valeur absolue la plus petite entre les 2 rayons (distance générale, ne pdonne pas la direction)
+        '''
+        return ( (abs(objet1.x-objet2.x) + abs(objet1.y-objet2.y)) - (objet1.rayon + objet2.rayon) )
+
 #tests de Haya
 
 #env1=Environnement(10,5,10)
