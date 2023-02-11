aeeb125f52398b68e9176540fa3796c894287fac
Elisa Li <elisa.li@hotmail.fr>
2023-02-11 17:40:50 +0100

    modification de deplacer avec vitesse angulaire

--- classes/Objet.py
+++ classes/Objet.py
@@ -1,9 +1,7 @@
 import math
+import Roue
 class Robot:
-  WHEEL_BASE_WIDTH         = 117  # distance (mm) de la roue gauche a la roue droite.
-  WHEEL_DIAMETER           = 66.5 #  diametre de la roue (mm)
-  WHEEL_BASE_CIRCUMFERENCE = WHEEL_BASE_WIDTH * math.pi # perimetre du cercle de rotation (mm)
-  WHEEL_CIRCUMFERENCE      = WHEEL_DIAMETER   * math.pi # perimetre de la roue (mm)
+
   
   def __init__(self, vitesseRoueGauche, vitesseRoueDroite, rayon):
     '''Constructeur de la classe Robot,représentation sous forme de cercle avec des coordonnées par défaut le coin haut gauche (rayon+0.1, rayon+0.1) , vitesse de la roue gauche et droite, un rayon une orientation (angle en radian), une vitesse max et min
@@ -15,20 +13,37 @@ class Robot:
     self.y = 0.1+rayon
     self.rayon = rayon
     self.orientation=0
+    self.distanceRoues=117
+    self.diametreRoue=66.5
+    self.perimetreRotation =self.distanceRoues * math.pi # perimetre du cercle de rotation (mm)
+    self.perimetreRoue = self.diametreRoue * math.pi 
+    self.roueGauche=Roue(self.diametreRoue/2)
+    self.roueDroite=Roue(self.diametreRoue/2)
     self.vitesseRoueDroite=vitesseRoueDroite
     self.vitesseRoueGauche=vitesseRoueGauche
+    self.vitesseAngulaireDroite=self.roueDroite.vitesseAngulaire(vitesseRoueDroite)
+    self.vitesseAngulaireGauche=self.roueGauche.vitesseAngulaire(vitesseRoueGauche)
 
   def setVitesse(self, vRoueGauche, vRoueDroite):
     """Set la vitesse des roues"""
     self.vitesseRoueDroite=vRoueDroite
     self.vitesseRoueGauche=vRoueGauche
 
-  def deplacer(self,delta_t):
-    """
-    Change les coordonnées x et y du robot selon sa vitesse et son angle avec un pas de temps
-    """
-    self.x +=(self.vitesseRoueGauche+self.vitesseRoueDroite)/2 * math.cos(self.orientation)*delta_t #vitesse linéare moyenne du robot
-    self.y +=(self.vitesseRoueGauche+self.vitesseRoueDroite)/2 * math.sin(self.orientation)*delta_t
+
+  #def deplacer(self,delta_t):
+  #  """
+  #  Change les coordonnées x et y du robot selon sa vitesse et son angle avec un pas de temps
+  #  """
+  # self.x +=(self.vitesseRoueGauche+self.vitesseRoueDroite)/2 * math.cos(self.orientation)*delta_t #vitesse linéare moyenne du robot
+  # self.y +=(self.vitesseRoueGauche+self.vitesseRoueDroite)/2 * math.sin(self.orientation)*delta_t
+
+  def deplacer(self, vitesseAngulaireDroite, vitesseAngulaireGauche, delta_t):
+    vitesseAngulaire = (self.diametreRoue/2 * (vitesseAngulaireDroite - vitesseAngulaireGauche)) / self.distanceRoues
+        
+    self.x += (self.vitesseRoueGauche+self.vitesseRoueDroite)/2 * math.cos(self.orientation)*delta_t 
+    self.y += (self.vitesseRoueGauche+self.vitesseRoueDroite)/2 * math.sin(self.orientation)*delta_t
+    self.theta += vitesseAngulaire * delta_t
+
 
   def distanceParcourue(self,delta_t):
     old_x=self.x
