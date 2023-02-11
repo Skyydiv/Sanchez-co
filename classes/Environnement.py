e4026142caf8e75b024c749ec1449372d6d58c95
Elisa Li <elisa.li@hotmail.fr>
2023-02-11 16:19:52 +0100

    ancienne version d'env

--- classes/Environnement.py
+++ classes/Environnement.py
@@ -1,3 +1,4 @@
+
 import numpy
 import random
 from Objet import Robot
@@ -33,13 +34,19 @@ class Environnement() :
         return False
     
     def estRobot(self,x, y,r):
+        """Verifie si les coordonnées passées en parametre se trouve sur la surface du robot
+        :param x: coordonnées en x
+        :param y: coordonnées en y
+        :param r:rayon en mm
+        """
         if(self.robot.x+self.robot.rayon>=x-r and self.robot.x-self.robot.rayon<=x+r and self.robot.y+self.robot.rayon>=y-r and self.robot.y-self.robot.rayon<=y+r):
             return True
 
     def estObstacle(self, x, y,r):
         '''Verfie s'il y a un obstacle dans l'environnement avec les mêmes coordonnées
-        :param x: indice de la ligne
-        :param y: indice de la colonne
+        :param x: coordonnées en x
+        :param y: coordonnées en y
+        :param r:rayon en mm
         :returns: True s'il existe déjà un obstacle avec les mêmes coordonnées, False sinon
         '''
         for i in self.ensemble_obstacles:
@@ -67,64 +74,13 @@ class Environnement() :
         :return : valeur négative ou égale à 0 si les objects sont en collision (ne gère pas la hauteur)
         :return : sinon valeur positive correspondant à la distance en valeur absolue la plus petite entre les 2 rayons (distance générale, ne pdonne pas la direction)
         '''
-<<<<<<< HEAD
-<<<<<<< HEAD
-        return ((abs(objet1.x-objet2.x) + abs(objet1.y-objet2.y)) - (objet1.rayon + objet2.rayon))
-=======
-        return ( (abs(objet1.x-objet2.x) + abs(objet1.y-objet2.y)) - (objet1.rayon + objet2.rayon) )
-=======
 
         return ((abs(objet1.x-objet2.x) + abs(objet1.y-objet2.y)) - (objet1.rayon + objet2.rayon))
-<<<<<<< HEAD
-        
->>>>>>> f1e1920a0a456af9bbb91ce138fbf1f5d78df641
-=======
 
->>>>>>> dbfd0c13210b359038929c493ecc6fb28dfdfc0f
     
     def detectCollision(self):
         '''
         verifie si les coordonnes du robot sont identiques a un obstacle de l’environnement ou s’il a pris un mur selon une precision
         '''
-<<<<<<< HEAD
-        if self.estObstacle(Robot.x,Robot.y) or self.estMur(Robot.x,Robot.y):
-<<<<<<< HEAD
-            return " Collision detectée"
-        
-        
->>>>>>> 590e454113df826660f053194898b6ffc949aa2a
-
-#tests de Haya
-
-#env1=Environnement(10,5,10)
-#robot1=Robot([6,4])
-
-#test estMur
-#assert(env1.estMur(2,-1)==True)
-
-#test addObstacle
-#env1.addObstacle(4.14,3.6,1,0)
-#assert(env1.tab[4][3]!=set())
-
-#test estObstacle
-#assert(env1.estObstacle(4.14,3.6)==True)
-
-#test addRobot
-#env1.addRobot(robot1)
-#assert(robot1 in env1.tab[0][0])
-
-#test deplacer
-#env1.deplacer(robot1)
-#assert(robot1.x==6.1 and robot1.y==4.1)
-
-#test distToCase
-# print(env1.distToCase(env1.echelle,robot1.x,robot1.y))
-
-
-=======
-            return " Collision detectée"
->>>>>>> f1e1920a0a456af9bbb91ce138fbf1f5d78df641
-=======
         return self.estObstacle(self.robot.x,self.robot.y,self.robot.rayon) or self.estMur(self.robot.x,self.robot.y,self.robot.rayon)
             
->>>>>>> dbfd0c13210b359038929c493ecc6fb28dfdfc0f
