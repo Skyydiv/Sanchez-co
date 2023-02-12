ddba41b8ed7e9d87fd3010204e209ecd9c2c9d0a
Haya Mamlouk <hayamamlouk25@gmail.com>
2023-02-12 11:39:00 +0100

    delete comments in lancementSimulation

--- classes/LancementSimualtion.py
+++ classes/LancementSimualtion.py
@@ -9,10 +9,7 @@ rob=Robot(4,4,30) # initialiser le robot
 env=Environnement([1000,500],rob,5) # initialiser l'environment
 simulation = Simulation(10000,env)
 simulation.addSimulation(5) #ajouter des obstcales a l'environnement
-
-#simulation.start() #lancer la simulation
-    
+  
 root = Tk() # initialiser la fenetre tkinter
 View(root, simulation)
 root.mainloop()
-simulation.stop()
