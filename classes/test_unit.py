576a7f3e43a245660f7ea73198ef3456614ff238
Hamza0u <aka.hamza02@gmail.com>
2023-02-14 21:33:55 +0100

    Ajout des tests pour les nouvelles methodes(setVitesse,set_motor_dps,test_vitesse_angulaire) et supression desanciennes methodes

--- classes/test_unit.py
+++ classes/test_unit.py
@@ -1,55 +1,35 @@
 import unittest
 
+from Roue import Wheel
 from Objet import Robot
 from Objet import Obstacle 
 from Environnement import *
 
 
+
+
+class TestRoue(unittest.TestCase):
+    def test_vitesse_angulaire(self):
+        roue=Wheel(2)
+        self.assertEqual(roue.vitesse_angulaire(10),5)
+
 class TestRobot(unittest.TestCase):
         
     def test_r_is_instanceof_Robot(self):
-        r=Robot(4,3,10);
+        r=Robot(4)
         self.assertIsInstance(r,Robot)
         
-    def test_setVitesseRoueGauche(self):
-        r=Robot(1,3,10);
-        r.setVitesseRoueGauche(6)
-        self.assertEqual(r.vitesseRoueGauche,6)
-        
-    def test_setVitesseRoueGauche(self):
-        r=Robot(1,3,10)
-        r.setVitesseRoueDroite(2)
-        self.assertEqual(r.vitesseRoueDroite,2)
-
-    
-    def test_tournerDroite(self):
-        r=Robot(1,3,10)
-        r.tournerDroite()
-        self.assertEqual(r.vitesseRoueDroite,0)
-            
-    def test_tournerGauche(self):
-        r=Robot(1,3,10)
-        r.tournerGauche()
-        self.assertEqual(r.vitesseRoueGauche,0)
-        
-    def test_arret(self):
-        r=Robot(1,3,10)
-        r.arret()
-        self.assertEqual(r.vitesseRoueGauche,0)
-        self.assertEqual(r.vitesseRoueDroite,0)
-    
-    def test_changerVitesse(self):
-        r=Robot(1,3,10)
-        r.changerVitesse(5,6)
+    def test_setVitesse(self):
+        r=Robot(4)
+        r.setVitesse(6,5)
         self.assertEqual(r.vitesseRoueGauche,5)
         self.assertEqual(r.vitesseRoueDroite,6)
-    
-    def test_deplacer(self):
-         r=Robot(4,3,20)
-         self.assertEqual(r.x,0.1)
-         r.deplacer()
-         self.assertEqual(r.x,3.6)
-         self.assertEqual(r.y,0.1)
+        
+    def test_set_motor_dps(self):
+         r=Robot(4)
+         r.set_motor_dps(None, 10)
+         self.assertEqual(r.MOTOR_RIGHT,10)
+         self.assertEqual(r.MOTOR_LEFT,10)
         
 class TestObstacle(unittest.TestCase):
     
@@ -63,26 +43,30 @@ class TestObstacle(unittest.TestCase):
 class TestEnvironnement(unittest.TestCase):
     
     def setUp(self):
-        robot=Robot(1,3,5)
+        robot=Robot(5)
         self.env=Environnement([200,200],robot,2)
         self.env.addObstacle(140,20,5,0,3)
         obs1=Obstacle(140,20,5,0,3)
         obs2=Obstacle(80,170,5,0,3)
         self.env.addObstacle(80,170,5,0,3)
+        self.env.addObstacle(10,10,5,0,3)
        
     def test_env_is_instanceof_Environnement(self):
         self.assertIsInstance(self.env,Environnement)
         
     def test_estMur(self):
         
-        self.assertTrue(self.env.estMur(2,-1))
-        self.assertFalse(self.env.estMur(100,76))
+        self.assertTrue(self.env.estMur(2,-1,5))
+        self.assertFalse(self.env.estMur(100,76,5))
     
     def test_estObstacle(self):
-        self.assertFalse(self.env.estObstacle(140,21))
-        self.assertTrue(self.env.estObstacle(140,20))
-        self.assertTrue(self.env.estObstacle(80,170))
-        self.assertFalse(self.env.estObstacle(80,171))
+        self.assertTrue(self.env.estObstacle(140,21,5))
+        self.assertTrue(self.env.estObstacle(140,20,5))
+        self.assertTrue(self.env.estObstacle(80,170,5))
+        self.assertTrue(self.env.estObstacle(80,171,5))
+        self.assertTrue(self.env.estObstacle(10,10,5))
+        self.assertTrue(self.env.estObstacle(12,12,5))
+        self.assertFalse(self.env.estObstacle(40,40,1))
         
     def test_calculDistance(self):
         obs1=Obstacle(140,20,5,0,3)
@@ -94,9 +78,9 @@ class TestSimulation(unittest.TestCase):
     
     
     def test_simu_instanceof_Simulation(self):
-        r=Robot(1,3,5)
+        r=Robot(5)
         envi=Environnement([200,200],r,2)
-        simu=Simulation(50,envi)
+        simu=Simulation(envi,50)
     
         self.assertIsInstance(simu,Simulation)
     
