dc6a029c9d2f8192ec7c1757c4d528c715c32452
Elisa Li <elisa.li@hotmail.fr>
2023-03-03 18:42:37 +0100

    package et module

--- Projet/Robot/simu/__init__.py
+++ Projet/Robot/simu/__init__.py
@@ -1,4 +1,3 @@
 from .objet import Robot, Obstacle, Environnement
 from .simulation import Simulation
 from .capteur import Capteur, dist, equationDroitePolaire, intersectionDroiteCercle, plusProche
-
