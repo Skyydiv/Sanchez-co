dd0162f1e45ab33bab2198b8bd066e5ea2abc58e
Elisa Li <elisa.li@hotmail.fr>
2023-03-03 16:20:33 +0100

    ajout du setup.py pour l'installation des packages

---
+++ Projet/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='Projet',
+   version='1.0',
+   description='Projet LU2IN013',
+   url='https://github.com/Skyydiv/Sanchez-co.git',
+   author='Sanchez & co',
+   author_email='',
+   packages=['Robot', 'test']
+)
