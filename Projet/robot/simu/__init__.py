6d24ea9e36b36c910790e77a31d24af7d6522785
Elisa Li <elisa.li@hotmail.fr>
2023-03-17 11:43:55 +0100

    renommer objet en virtuel

--- Projet/robot/simu/__init__.py
+++ Projet/robot/simu/__init__.py
@@ -1,3 +1,3 @@
-from .objet import Robot, Obstacle, Environnement
+from .virtuel import Robot, Obstacle, Environnement
 from .simulation import Simulation
 from .capteur import Capteur, dist, equationDroitePolaire, intersectionDroiteCercle, plusProche
