dc17ffceb3fb913af4191938e116687225d156da
Hamza0u <aka.hamza02@gmail.com>
2023-03-29 09:40:00 +0200

    test de ia carre

--- Projet/main.py
+++ Projet/main.py
@@ -14,22 +14,23 @@ simulation=Simulation(delta_simu)
 simulation.addSimulation(0) #ajouter des obstcales a l'environnement
 
 #ajout du controleur
-
 cr=ControleurRobotVirtuel(simulation.robot)
 
-ia1=Ia_Avancer_tout_droit(100,100,cr)
-iacarre=IAcarre(cr,100,100,200,delta_ia)
+
+#ia pour avancer tout droit 
+ia1=Ia_Avancer_tout_droit(300,200,cr)
+
+#ia pour tourner 
 iaa=IATournerAngle(cr,90,200)
+
+#ia seq 
 iaseq=IAseq(cr,[ia1,iaa,ia1,iaa,ia1,iaa,ia1])
-iaboucle=BoucleIA(cr,iacarre,delta_ia)
-iaboucle.start()
 
-#iacarre=IAcarre(cr,100,100,200,delta_ia)
-#iacarre.start()
+#iaboucle=BoucleIA(cr,iaseq,delta_ia)
+#iaboucle.start()
 
-
-
-# iaboucle.start()
+iacarre=IAcarre(cr,100,100,200,delta_ia)
+iacarre.start()
 
 root = Tk() # initialiser la fenetre tkinter
 view=View(root, simulation,delta_affich)
