e3985b7fdff70b2815f0205b4c0162fbacff1b3c
Haya Mamlouk <hayamamlouk25@gmail.com>
2023-04-04 14:52:34 +0200

    rendre le main plus lisible en mettant tout dans les innit et en gardant que le lancement dans le main

--- Projet/main.py
+++ Projet/main.py
@@ -1,38 +1,8 @@
-from tkinter import *
-from robot.affichage import View
-from robot.simu.simulation import Simulation
-from robot.IA import Ia_Avancer_tout_droit, IATournerAngle, BoucleIA, IAseq, TracerCarre, IAIfThenElse,IAevitecrash 
-from robot.IA.controleur import ControleurRobotVirtuel, ControleurRobotVraieVie
-from time import sleep
+from robot.simu import simulation
+from robot.IA import iaboucle 
+from robot.affichage import root
 
-
-delta_simu=60000#taux rafraichissement des données de la simu
-delta_affich=1 #taux de rafraichissement de l'affichage
-delta_ia=0.000000000001
-
-simulation=Simulation(delta_simu)
-simulation.addSimulation(0) #ajouter des obstcales a l'environnement
-
-#ajout du controleur
-cr=ControleurRobotVirtuel(simulation.robot)
-
-
-#ia pour avancer tout droit 
-ia1=Ia_Avancer_tout_droit(300,200,cr)
-
-#ia pour tourner 
-iaa=IATournerAngle(cr,90,200)
-
-#ia seq 
-Carre=TracerCarre(cr,300,200)
-iaseq=IAseq(cr,[Carre,Carre])
-
-
-iaboucle=BoucleIA(cr,iaseq,delta_ia)
 iaboucle.start()
-
-root = Tk() # initialiser la fenetre tkinter
-view=View(root, simulation,delta_affich)
 simulation.run_simu()
 root.mainloop()
 simulation.stop_simu()
