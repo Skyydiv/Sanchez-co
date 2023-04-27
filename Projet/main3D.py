00bced96106d2d15f268125fc92b1f70b80ab4e8
Skyydiv <Corentin-fontaine@hotmail.com>
2023-04-27 21:50:09 +0200

    Ajout des obstacles à la 3D + changement léger de l'échelle

--- Projet/main3D.py
+++ Projet/main3D.py
@@ -3,12 +3,16 @@ from robot.IA import ControleurRobotVirtuel,get_Tracerarre, get_AvancerToutDroit
 from robot.affichage import create_aff3D
 
 #le client fait son choix avec le nombre d'obstacle
-simulation=create_simu(0)
+simulation=create_simu(4)
+# simulation.environnement.addObstacle(600,0,0,0,30)
+# simulation.environnement.addObstacle(0,simulation.SIMU_HEIGHT,0,0,30)
+# simulation.environnement.addObstacle(simulation.SIMU_WIDTH,0,0,0,30)
+# simulation.environnement.addObstacle(simulation.SIMU_WIDTH,simulation.SIMU_HEIGHT,0,0,30)
 affichage=create_aff3D(simulation)
 cr=ControleurRobotVirtuel(simulation.robot)  #initialiser le controleur  virtuel
 
 #le client fait son choix de la strategie qui souhaite executé
-strategie=get_Tracerarre(cr,400,300)
+strategie=get_Tracerarre(cr,500,300)
 #De plus, le client decide s'il veut repeter cette startegie plusieurs fois,avec le nombre de repetition
 iaseq=get_Seq_IA(cr,10,strategie)
 #Le client decide quelle stratgie lancer
--- Projet/robot/affichage/view3D.py
+++ Projet/robot/affichage/view3D.py
@@ -1,4 +1,4 @@
-from math import pi, sin, cos, degrees
+from math import pi, sin, cos, degrees, sqrt
 
 
 from direct.showbase.ShowBase import ShowBase
@@ -20,6 +20,7 @@ class View3D(ShowBase):
         
 
         self.simu=simulation
+        self.env=simulation.environnement
         self.robot=simulation.robot
         self.delta_affichage=delta_affichage/100000
         
@@ -34,17 +35,37 @@ class View3D(ShowBase):
 
         # Apply scale and position transforms on the model.
         # self.scene.setScale(0.25, 0.25, 0.25)
-        self.scene.setScale(10, 10, 10)
-        self.scene.setPos(200, 2000, 0)
+        self.scene.setScale(11, 11, 11)
+        self.scene.setPos(440, 2000, 0)
         
 
+            #Obstacles
+        obs=[]
+        i=0
+        for ob in self.env.ensemble_obstacles:
+            obs.append(self.loader.loadModel("3Dmodels/rock/scene.gltf"))
+            # Reparent the model to render.
+            obs[i].reparentTo(self.render)
+            obs[i].setPos(ob.x, ob.y, 0)
+            nb=int(sqrt(ob.rayon)*3)
+            obs[i].setScale(nb, nb, nb)
+            obs[i].setHpr(0, 90, 0)
+            i += 1
+
+        # self.ob1 = self.loader.loadModel("rock/scene.gltf")
+        # obs.append(self.ob1)
+        # # Reparent the model to render.
+        # self.ob1.reparentTo(self.render)
+        # self.ob1.setPos(0, 0, 0)
+        # self.ob1.setScale(30, 30, 30)
+        # self.ob1.setHpr(0, 90, 0)
 
             #MODELS
 
         # Load and transform the panda actor.
         self.pandaActor = Actor("models/panda-model",
                                 {"walk": "models/panda-walk4"})
-        self.pandaActor.setScale(0.2, 0.2, 0.2)
+        self.pandaActor.setScale(0.15, 0.15, 0.15)
         self.pandaActor.reparentTo(self.render)
         # self.pandaActor.setPos(12, 13, 0)
 
@@ -56,12 +77,12 @@ class View3D(ShowBase):
         #CAMERA-------------------------------------------------------------------------------------------------------------------------------------------------------
         
         #ajout d'une tache camera pour avoir une vue FPS---------------------------------------------------------------------------------------------------------------
-        # self.taskMgr.add(self.spinCameraTaskFPS, "SpinCameraTaskFPS")
+        #self.taskMgr.add(self.spinCameraTaskFPS, "SpinCameraTaskFPS")
         
         #ajout d'une tache camera pour avoir une vue TPS---------------------------------------------------------------------------------------------------------------
         self.taskMgr.add(self.spinCameraTaskTPS, "SpinCameraTaskTPS")
 
-
+  
         #crée un thread pour lancer la fonction updatePos: changer la position du robot
         self.updatePosTH=threading2.Thread(name="updatePosRob",target=self.updatePosRob,args=[],daemon=True)
         print("POSTH",self.updatePosTH.name)
@@ -93,7 +114,7 @@ class View3D(ShowBase):
         # Orienter la caméra vers l'avant en utilisant l'orientation actuelle du panda
         lookAtX = self.robot.x + (600* cos(self.robot.orientation))
         lookAtY = self.robot.y + (600*sin(self.robot.orientation))
-        lookAtZ = 80
+        lookAtZ = 70
         self.camera.lookAt(lookAtX, lookAtY, lookAtZ)
         
         
@@ -105,10 +126,7 @@ class View3D(ShowBase):
     # Define a procedure to move the camera.
     #set les valeurs de la caméra (position et orientation)
     def spinCameraTaskTPS(self, task):
-       self.camera.setPos(352, -1376.4, 1855)
+       self.camera.setPos(700, -1376.4, 1855)
        self.camera.setHpr(-2.5, -46.2, 0.5)
        return Task.cont
     
-
-
-
